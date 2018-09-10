d7b0eab24490e53204adf572eedd581be6abb9f9
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2018-09-10 19:48:48 -0400

    addded import

---
+++ main.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+testDataSet = pd.read_csv("C:/Users/gondolin\PycharmProjects/MLTitanic/DescionTreePythonML/data/test.csv")
+trainDataSet = pd.read_csv("C:/Users/gondolin\PycharmProjects/MLTitanic/DescionTreePythonML/data/train.csv")
