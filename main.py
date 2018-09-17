155fa44d3421277e00f2e082bb2e81a8d4aa0f65
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2018-09-17 00:47:17 -0400

    adding prediction

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
 import pandas as pd
 from sklearn import tree
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing
+import numpy as np
+from sklearn.metrics import accuracy_score
 
 # Read from a csv and set index column
 trainDataSet = pd.read_csv("C:/Users/gondolin/PycharmProjects/MLTitanic/DescionTreePythonML/data/train.csv",
                            index_col='PassengerId')
 trainDataSet = trainDataSet[trainDataSet.columns.tolist()]
+
+# Data Sanataize
 trainDataSet['Sex'] = trainDataSet['Sex'].map({'male': 0, 'female': 1})
 trainDataSet['Embarked'] = trainDataSet['Embarked'].map({'C': 0, 'Q': 1, 'S': 2})
-
 trainDataSet.dropna()
-# remove non needed columns
 X = trainDataSet.drop('Survived', axis=1)
-# print(X.get('Embarked'))
-# X = X.drop('Cabin', axis=1)
-# X = X.drop('Cabin', axis=1)
+X = X.drop('Name', axis=1)  # not needed
+X = X.drop('Ticket', axis=1)  # not needed
+X = X.drop('Cabin', axis=1)  # not needed
+
+AgeCol = X.get('Age')
+intCount = 0
+intSum = 0
+
+for passAge in AgeCol:
+    if ~np.isnan(passAge):
+        intSum = passAge + intSum
+        intCount = intCount + 1
+averageAge = intSum/intCount
+
+
+# trainDataSet['Age'] = trainDataSet['Age'].map({'nan': averageAge})
+X = X.drop('SibSp', axis=1)  # not needed
+X = X.drop('Parch', axis=1)  # not needed
+X = X.drop('Embarked', axis=1)  # not needed
+X = X.drop('Fare', axis=1)  # not needed
+X = X.drop('Age', axis=1)  # not needed
+
 print(X.head(n=0))
 y = trainDataSet['Survived']
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
 model = tree.DecisionTreeClassifier()
-# model.fit(X_train, y_train)
+model.fit(X_train, y_train)
+y_predict = model.predict(X_test)
+print(accuracy_score(y_test, y_predict))
