936478434816ac947dd9013baedf016e9cbe6b69
Eduardo Guzman-Diaz <tloz_tlotr@hotmail.com>
2018-09-16 22:12:03 -0400

    traform mapping

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from sklearn import tree
+from sklearn.model_selection import train_test_split
 
 # Read from a csv and set index column
-trainDataSet = pd.read_csv("C:/Users/gondolin/PycharmProjects/MLTitanic/DescionTreePythonML/data/train.csv", index_col='PassengerId')
-
+trainDataSet = pd.read_csv("C:/Users/gondolin/PycharmProjects/MLTitanic/DescionTreePythonML/data/train.csv",
+                           index_col='PassengerId')
+trainDataSet = trainDataSet[trainDataSet.columns.tolist()]
+trainDataSet['Sex'] = trainDataSet['Sex'].map({'male': 0, 'female': 1})
